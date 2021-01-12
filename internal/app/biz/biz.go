f2b40de2b00604cc06d7a1f98715dcdf1bdfde01
Sean Cheng <blackhorseya@gmail.com>
2021-01-13 07:33:46 +0800

    fix: remove okr biz

--- internal/app/biz/biz.go
+++ internal/app/biz/biz.go
@@ -1,13 +1,11 @@
 package biz
 
 import (
-	"github.com/blackhorseya/lobster/internal/app/biz/okr"
 	"github.com/blackhorseya/lobster/internal/app/biz/todo"
 	"github.com/google/wire"
 )
 
 // ProviderSet is a provider set for wire
 var ProviderSet = wire.NewSet(
-	okr.ProviderSet,
 	todo.ProviderSet,
 )
