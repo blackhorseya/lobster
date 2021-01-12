591d609a9b429241f71a232f27bc8949dd549154
Sean Cheng <blackhorseya@gmail.com>
2021-01-12 14:17:04 +0800

    feat: added init logger
    - added init logger in main file

--- cmd/lobster/main.go
+++ cmd/lobster/main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"flag"
+	"strings"
 
+	"github.com/blackhorseya/lobster/internal/pkg/config"
 	"github.com/sirupsen/logrus"
 )
+
+const timeFormat = "2006-01-02 15:04:05"
 
 var path = flag.String("c", "configs/app.yaml", "set config file path")
 
 func init() {
 	flag.Parse()
+}
+
+func initLogger(cfg *config.Config) {
+	if level, err := logrus.ParseLevel(cfg.Log.Level); err != nil {
+		logrus.SetLevel(logrus.InfoLevel)
+		logrus.Warn(err)
+	} else {
+		logrus.SetLevel(level)
+	}
+
+	switch strings.ToLower(cfg.Log.Format) {
+	case "json":
+		logrus.SetFormatter(&logrus.JSONFormatter{
+			TimestampFormat: timeFormat,
+		})
+	default:
+		logrus.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp:   true,
+			TimestampFormat: timeFormat,
+			DisableQuote:    true,
+		})
+	}
 }
 
 // @title Lobster API
@@ -32,6 +58,8 @@ func main() {
 	if injector == nil {
 		return
 	}
+
+	initLogger(injector.C)
 
 	err = injector.Engine.Run(injector.C.HTTP.GetAddress())
 	if err != nil {
