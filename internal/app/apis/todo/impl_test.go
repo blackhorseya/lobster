eed234d1547fe4b840e988be00cd9438a1c88cfd
Sean Cheng <blackhorseya@gmail.com>
2021-01-14 07:57:34 +0800

    test: added todo handler test file

---
+++ internal/app/apis/todo/impl_test.go
@@ -0,0 +1,39 @@
+package todo
+
+import (
+	"testing"
+
+	"github.com/blackhorseya/lobster/internal/app/biz/todo/mocks"
+	"github.com/blackhorseya/lobster/internal/pkg/transports/http/middlewares"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/suite"
+)
+
+type handlerSuite struct {
+	suite.Suite
+	r       *gin.Engine
+	mock    *mocks.IBiz
+	handler IHandler
+}
+
+func (s *handlerSuite) SetupTest() {
+	gin.SetMode(gin.TestMode)
+	s.r = gin.New()
+	s.r.Use(middlewares.ContextMiddleware())
+	s.r.Use(middlewares.LoggerMiddleware())
+
+	s.mock = new(mocks.IBiz)
+	if handler, err := CreateIHandler(s.mock); err != nil {
+		panic(err)
+	} else {
+		s.handler = handler
+	}
+}
+
+func (s *handlerSuite) TearDownTest() {
+	s.mock.AssertExpectations(s.T())
+}
+
+func TestHandlerSuite(t *testing.T) {
+	suite.Run(t, new(handlerSuite))
+}
---
+++ internal/app/apis/todo/wire.go
@@ -0,0 +1,15 @@
+// +build wireinject
+
+package todo
+
+import (
+	"github.com/blackhorseya/lobster/internal/app/biz/todo"
+	"github.com/google/wire"
+)
+
+var testProviderSet = wire.NewSet(NewImpl)
+
+// CreateIHandler serve caller to create an IHandler
+func CreateIHandler(biz todo.IBiz) (IHandler, error) {
+	panic(wire.Build(testProviderSet))
+}
