1fabb9a44de718a1c2b93245ee1a2cbfe6524b1e
Sean Cheng <blackhorseya@gmail.com>
2021-01-18 22:04:35 +0800

    feat: added impl for okr biz

--- internal/app/biz/okr/okr.go
+++ internal/app/biz/okr/okr.go
@@ -1,6 +1,7 @@
 package okr
 
 import (
+	"github.com/blackhorseya/lobster/internal/app/biz/okr/repo"
 	"github.com/blackhorseya/lobster/internal/pkg/contextx"
 	"github.com/blackhorseya/lobster/internal/pkg/entities/biz/okr"
 	"github.com/google/wire"
@@ -22,16 +23,7 @@ type IBiz interface {
 
 	// DeleteObjective serve user to delete a objective by id
 	DeleteObjective(ctx contextx.Contextx, id string) error
-
-	// SetResult serve user to create a key result for objective
-	SetResult(ctx contextx.Contextx, id string, result *okr.KeyResult) (*okr.KeyResult, error)
-
-	// UpdateResult serve user to update a key result
-	UpdateResult(ctx contextx.Contextx, id string, updated *okr.KeyResult) (*okr.KeyResult, error)
-
-	// DeleteResult serve user to delete a key result by id
-	DeleteResult(ctx contextx.Contextx, id string) error
 }
 
 // ProviderSet is a provider set for wire
-var ProviderSet = wire.NewSet()
+var ProviderSet = wire.NewSet(NewImpl, repo.ProviderSet)
