09819605411a6a9e6efe57c63055abc1b90bb06d
Sean Cheng <blackhorseya@gmail.com>
2021-01-19 20:45:30 +0800

    feat: implement objective repo functions

--- internal/app/biz/objective/repo/impl.go
+++ internal/app/biz/objective/repo/impl.go
@@ -1,9 +1,13 @@
 package repo
 
 import (
+	"time"
+
 	"github.com/blackhorseya/lobster/internal/pkg/contextx"
 	"github.com/blackhorseya/lobster/internal/pkg/entities/biz/okr"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type impl struct {
@@ -15,32 +19,100 @@ func NewImpl(mongo *mongo.Client) IRepo {
 	return &impl{mongo: mongo}
 }
 
-func (i *impl) QueryByID(ctx contextx.Contextx, id string) (*okr.Objective, error) {
-	// todo: 2021-01-19|11:20|doggy|implement me
-	panic("implement me")
+func (i *impl) Create(ctx contextx.Contextx, created *okr.Objective) (*okr.Objective, error) {
+	timeout, cancel := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	coll := i.mongo.Database("lobster-db").Collection("objectives")
+	_, err := coll.InsertOne(timeout, created)
+	if err != nil {
+		return nil, err
+	}
+
+	return created, nil
 }
 
-func (i *impl) Create(ctx contextx.Contextx, created *okr.Objective) (*okr.Objective, error) {
-	// todo: 2021-01-19|11:20|doggy|implement me
-	panic("implement me")
+func (i *impl) QueryByID(ctx contextx.Contextx, id string) (*okr.Objective, error) {
+	timeout, cancel := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	coll := i.mongo.Database("lobster-db").Collection("objectives")
+	res := coll.FindOne(timeout, bson.D{{"id", id}})
+	if res.Err() != nil {
+		return nil, res.Err()
+	}
+
+	var ret *okr.Objective
+	if err := res.Decode(&ret); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
 
 func (i *impl) List(ctx contextx.Contextx, offset, limit int) ([]*okr.Objective, error) {
-	// todo: 2021-01-19|11:20|doggy|implement me
-	panic("implement me")
+	timeout, cancel := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	coll := i.mongo.Database("lobster-db").Collection("objectives")
+	cur, err := coll.Find(timeout, bson.D{}, options.Find().SetSkip(int64(offset)).SetLimit(int64(limit)))
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(timeout)
+
+	var ret []*okr.Objective
+	for cur.Next(timeout) {
+		var ele *okr.Objective
+		if err := cur.Decode(&ele); err != nil {
+			return nil, err
+		}
+
+		ret = append(ret, ele)
+	}
+
+	if cur.Err() != nil {
+		return nil, cur.Err()
+	}
+
+	return ret, nil
 }
 
 func (i *impl) Count(ctx contextx.Contextx) (int, error) {
-	// todo: 2021-01-19|11:20|doggy|implement me
-	panic("implement me")
+	timeout, cancel := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	coll := i.mongo.Database("lobster-db").Collection("objectives")
+	ret, err := coll.CountDocuments(timeout, bson.D{})
+	if err != nil {
+		return 0, err
+	}
+
+	return int(ret), nil
 }
 
 func (i *impl) Update(ctx contextx.Contextx, updated *okr.Objective) (*okr.Objective, error) {
-	// todo: 2021-01-19|11:20|doggy|implement me
-	panic("implement me")
+	timeout, cancel := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	coll := i.mongo.Database("lobster-db").Collection("objectives")
+	_, err := coll.UpdateOne(timeout, bson.D{{"id", updated.ID}}, bson.D{{"$set", updated}})
+	if err != nil {
+		return nil, err
+	}
+
+	return updated, nil
 }
 
 func (i *impl) Delete(ctx contextx.Contextx, id string) (int, error) {
-	// todo: 2021-01-19|11:20|doggy|implement me
-	panic("implement me")
+	timeout, cancel := contextx.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	coll := i.mongo.Database("lobster-db").Collection("objectives")
+	_, err := coll.DeleteOne(timeout, bson.D{{"id", id}})
+	if err != nil {
+		return 0, err
+	}
+
+	return 1, nil
 }
--- internal/app/biz/objective/repo/impl_test.go
+++ internal/app/biz/objective/repo/impl_test.go
@@ -1,9 +1,32 @@
+// +build integration
+
 package repo
 
 import (
+	"reflect"
 	"testing"
 
+	"github.com/blackhorseya/lobster/internal/pkg/contextx"
+	"github.com/blackhorseya/lobster/internal/pkg/entities/biz/okr"
 	"github.com/stretchr/testify/suite"
+)
+
+var (
+	uuid1 = "d76f4f51-f141-41ba-ba57-c4749319586b"
+
+	time1 = int64(1611059529208050000)
+
+	obj1 = &okr.Objective{
+		ID:       uuid1,
+		Title:    "obj1",
+		CreateAt: time1,
+	}
+
+	updated1 = &okr.Objective{
+		ID:       uuid1,
+		Title:    "updated obj1",
+		CreateAt: time1,
+	}
 )
 
 type repoSuite struct {
@@ -21,4 +44,190 @@ func (s *repoSuite) SetupTest() {
 
 func TestRepoSuite(t *testing.T) {
 	suite.Run(t, new(repoSuite))
+}
+
+func (s *repoSuite) Test_impl_Create() {
+	type args struct {
+		created *okr.Objective
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *okr.Objective
+		wantErr bool
+	}{
+		{
+			name:    "created then obj nil",
+			args:    args{created: obj1},
+			want:    obj1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			got, err := s.repo.Create(contextx.Background(), tt.args.created)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Create() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func (s *repoSuite) Test_impl_QueryByID() {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *okr.Objective
+		wantErr bool
+	}{
+		{
+			name:    "uuid then obj1 nil",
+			args:    args{id: uuid1},
+			want:    obj1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			got, err := s.repo.QueryByID(contextx.Background(), tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("QueryByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("QueryByID() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func (s *repoSuite) Test_impl_List() {
+	type args struct {
+		offset int
+		limit  int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []*okr.Objective
+		wantErr bool
+	}{
+		{
+			name:    "0 1 then objs nil",
+			args:    args{offset: 0, limit: 1},
+			want:    []*okr.Objective{obj1},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			got, err := s.repo.List(contextx.Background(), tt.args.offset, tt.args.limit)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("List() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("List() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func (s *repoSuite) Test_impl_Count() {
+	type args struct {
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "count then 1 nil",
+			args:    args{},
+			want:    1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			got, err := s.repo.Count(contextx.Background())
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Count() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Count() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func (s *repoSuite) Test_impl_Update() {
+	type args struct {
+		updated *okr.Objective
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *okr.Objective
+		wantErr bool
+	}{
+		{
+			name:    "update then obj1 nil",
+			args:    args{updated: updated1},
+			want:    updated1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			got, err := s.repo.Update(contextx.Background(), tt.args.updated)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Update() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func (s *repoSuite) Test_impl_Delete() {
+	type args struct {
+		id string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "uuid then 1 nil",
+			args:    args{id: uuid1},
+			want:    1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		s.T().Run(tt.name, func(t *testing.T) {
+			got, err := s.repo.Delete(contextx.Background(), tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("Delete() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
