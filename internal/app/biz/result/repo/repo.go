23d4a08aab640cdbdfe46ca40d7c6d77179f29d3
Sean Cheng <blackhorseya@gmail.com>
2021-01-25 10:07:25 +0800

    docs: added todo list

--- internal/app/biz/result/repo/repo.go
+++ internal/app/biz/result/repo/repo.go
@@ -4,6 +4,7 @@ import "github.com/google/wire"
 
 // IRepo declare key result repo service function
 type IRepo interface {
+	// todo: 2021-01-25|10:07|doggy|implement me
 }
 
 // ProviderSet is a provider set for wire
--- internal/app/biz/result/result.go
+++ internal/app/biz/result/result.go
@@ -7,6 +7,7 @@ import (
 
 // IBiz declare key result biz service function
 type IBiz interface {
+	// todo: 2021-01-25|10:07|doggy|implement me
 }
 
 // ProviderSet is a provider set for wire
