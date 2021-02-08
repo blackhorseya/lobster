bd06468bfd9ed542d3eaf53e6f7077a67e138f15
Sean Cheng <blackhorseya@gmail.com>
2021-02-09 07:15:55 +0800

    feat: added objectives command

---
+++ internal/cmd/objectives/objectives.go
@@ -0,0 +1,63 @@
+package objectives
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/blackhorseya/lobster/internal/pkg/config"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	format = "%-36s\t%-20s\t%-6v\t%-9v"
+)
+
+var (
+	header = fmt.Sprintf(format, "ID", "Title", "Status", "Create At")
+)
+
+var (
+	cfgFile string
+
+	cfg *config.Config
+
+	// Cmd is root command
+	Cmd = &cobra.Command{
+		Use:   "objectives",
+		Short: "Objective management",
+	}
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".cli" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".lobster")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	_ = viper.ReadInConfig()
+
+	if err := viper.Unmarshal(&cfg); err != nil {
+		panic(err)
+	}
+}
