653da741cf4440a24d053dfe5903956f625d131a
Sean Cheng <blackhorseya@gmail.com>
2021-02-15 13:20:38 +0800

    feat: implement get by id krs api

---
+++ internal/apis/restful/kr/wire.go
@@ -0,0 +1,15 @@
+// +build wireinject
+
+package kr
+
+import (
+	"github.com/blackhorseya/lobster/internal/biz/kr"
+	"github.com/google/wire"
+)
+
+var testProviderSet = wire.NewSet(NewImpl)
+
+// CreateIHandler serve caller to create an IHandler
+func CreateIHandler(biz kr.IBiz) (IHandler, error) {
+	panic(wire.Build(testProviderSet))
+}
