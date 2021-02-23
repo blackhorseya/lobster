cf24db69a371db7f8ceaaaa323fcefc7dcdde2e0
Sean Cheng <blackhorseya@gmail.com>
2021-02-23 12:57:45 +0800

    fix: typo

--- internal/apis/cmd/get/get.go
+++ internal/apis/cmd/get/get.go
@@ -31,85 +31,6 @@ var (
 			size, _ := cmd.Flags().GetInt("size")
 
 			// todo: 2021-02-15|14:46|doggy|refactor it
-			if len(args) == 2 {
-				id := args[1]
-				uri := fmt.Sprintf("%v/v1/%v/%v", cfg.API.EndPoint, args[0], id)
-
-				req, err := http.NewRequest(http.MethodGet, uri, nil)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				client := &http.Client{}
-				resp, err := client.Do(req)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				defer func() {
-					_ = resp.Body.Close()
-				}()
-
-				if resp.StatusCode == http.StatusNotFound {
-					fmt.Println("No resource found")
-					return
-				}
-
-				body, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				switch args[0] {
-				case "tasks":
-					var task *pb.Task
-					err = json.Unmarshal(body, &task)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-
-					table := tablewriter.NewWriter(os.Stdout)
-					table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-					table.SetHeader([]string{"ID", "Result ID", "Title", "Status", "Create At"})
-					table.Append(task.ToLine())
-					table.Render()
-
-					break
-				case "goals":
-					var data *okr.Objective
-					err = json.Unmarshal(body, &data)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-
-					table := tablewriter.NewWriter(os.Stdout)
-					table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-					table.SetHeader([]string{"ID", "Title", "Start At", "End At", "Create At"})
-					table.Append(data.ToLine())
-					table.Render()
-
-					break
-				case "results":
-					var data *okr.KeyResult
-					err = json.Unmarshal(body, &data)
-					if err != nil {
-						fmt.Println(err)
-						return
-					}
-
-					table := tablewriter.NewWriter(os.Stdout)
-					table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-					table.SetHeader([]string{"ID", "Goad ID", "Title", "Target", "Actual", "Progress", "Create At"})
-					table.Append(data.ToLine())
-					table.Render()
-
-					break
-				}
-			}
 
 			if len(args) == 1 {
 				uri := fmt.Sprintf("%v/v1/%v?page=%v&size=%v", cfg.API.EndPoint, args[0], page, size)
@@ -183,7 +104,7 @@ var (
 
 					table := tablewriter.NewWriter(os.Stdout)
 					table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-					table.SetHeader([]string{"ID", "Goad ID", "Title", "Target", "Actual", "Progress", "Create At"})
+					table.SetHeader([]string{"ID", "Goal ID", "Title", "Target", "Actual", "Progress", "Create At"})
 					for _, t := range data {
 						table.Append(t.ToLine())
 					}
