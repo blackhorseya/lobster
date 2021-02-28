3926cd095ea5a3444dfa0aeb059770c558fa2889
Sean Cheng <blackhorseya@gmail.com>
2021-02-28 16:41:03 +0800

    feat: added user biz

---
+++ internal/biz/user/user.go
@@ -0,0 +1,24 @@
+package user
+
+import (
+	"github.com/blackhorseya/lobster/internal/pkg/contextx"
+	"github.com/blackhorseya/lobster/internal/pkg/pb"
+)
+
+// IBiz declare user biz service function
+type IBiz interface {
+	// GetInfoByID serve caller use userID to get UserInfo
+	GetInfoByID(ctx contextx.Contextx, id string) (info *pb.Profile, err error)
+
+	// GetInfoByEmail serve caller user userEmail to get UserInfo
+	GetInfoByEmail(ctx contextx.Contextx, email string) (info *pb.Profile, err error)
+
+	// GetInfoByAccessToken serve caller use user's AccessToken to get userInfo
+	GetInfoByAccessToken(ctx contextx.Contextx, token string) (info *pb.Profile, err error)
+
+	// Login serve user to login with userEmail, and user Password
+	Login(ctx contextx.Contextx, email, token string) (info *pb.Profile, err error)
+
+	// Logout serve user to logout application which he/she wants
+	Logout(ctx contextx.Contextx, user *pb.Profile) (err error)
+}
