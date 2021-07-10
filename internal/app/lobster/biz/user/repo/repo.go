9b97a3d9f4fad1a7ce2fda8745e1715ecf570f23
Sean Zheng <blackhorseya@gmail.com>
2021-07-11 06:54:14 +0800

    feat: added user repo

---
+++ internal/app/lobster/biz/user/repo/repo.go
@@ -0,0 +1,25 @@
+package repo
+
+import (
+	"github.com/blackhorseya/lobster/internal/pkg/base/contextx"
+	"github.com/blackhorseya/lobster/internal/pkg/entity/user"
+	"github.com/google/wire"
+)
+
+// IRepo declare user repo function
+type IRepo interface {
+	// GetByID serve caller to get user information by id
+	GetByID(ctx contextx.Contextx, id int64) (info *user.Profile, err error)
+
+	// GetByToken serve caller to get user information by token
+	GetByToken(ctx contextx.Contextx, token string) (info *user.Profile, err error)
+
+	// GetByEmail serve caller to get user information by email
+	GetByEmail(ctx contextx.Contextx, email string) (info *user.Profile, err error)
+
+	// Register serve caller to create an account by email and password
+	Register(ctx contextx.Contextx, email, password string) (info *user.Profile, err error)
+}
+
+// ProviderSet is a provider set for wire
+var ProviderSet = wire.NewSet()
